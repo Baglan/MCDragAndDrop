455ac07e3fdc382dc4f11af7c75e671e75680542
Baglan Dosmagambetov <baglan.dos@gmail.com>
2013-06-10 12:07:25 +0600

    Initial Commit

---
+++ MCDragAndDrop/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCDragAndDrop
+//
+//  Created by Baglan on 6/10/13.
+//  Copyright (c) 2013 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
